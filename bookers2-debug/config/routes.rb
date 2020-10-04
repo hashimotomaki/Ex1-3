8cd65a6720532b2d3d0d9c5a7d1cf5558f416273
EC2 Default User <ec2-user@ip-172-31-41-123.ec2.internal>
2020-10-04 15:53:49 +0000

    104

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :favorites,only: [:create,:destroy]
+  resources :book_comments,only: [:reate, :destroy]
 end
